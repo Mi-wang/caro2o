dee909851545e97a806cc4bc55d1e71cf2de5db5
Mi-Manchi <2046380350@qq.com>
2023-01-29 22:44:37 +0800

    营销任务crud

--- caro2o-admin-ui/src/views/workflow/tasks/index.vue
+++ caro2o-admin-ui/src/views/workflow/tasks/index.vue
@@ -1,29 +1,13 @@
 <template>
   <div class="app-container">
     <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
-      <el-form-item label="活动名称" prop="name">
+      <el-form-item label="任务名称" prop="name">
         <el-input
           v-model="queryParams.name"
-          placeholder="请输入活动名称"
+          placeholder="请输入任务名称"
           clearable
           @keyup.enter.native="handleQuery"
         />
-      </el-form-item>
-      <el-form-item label="创建时间" prop="creattime">
-        <el-date-picker clearable
-          v-model="queryParams.creattime"
-          type="date"
-          value-format="yyyy-MM-dd"
-          placeholder="请选择创建时间">
-        </el-date-picker>
-      </el-form-item>
-      <el-form-item label="终止时间" prop="stopTime">
-        <el-date-picker clearable
-          v-model="queryParams.stopTime"
-          type="date"
-          value-format="yyyy-MM-dd"
-          placeholder="请选择终止时间">
-        </el-date-picker>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
@@ -39,7 +23,7 @@
           icon="el-icon-plus"
           size="mini"
           @click="handleAdd"
-          v-hasPermi="['workflow:marketing:add']"
+          v-hasPermi="['workflow:tasks:add']"
         >新增</el-button>
       </el-col>
       <el-col :span="1.5">
@@ -50,7 +34,7 @@
           size="mini"
           :disabled="single"
           @click="handleUpdate"
-          v-hasPermi="['workflow:marketing:edit']"
+          v-hasPermi="['workflow:tasks:edit']"
         >修改</el-button>
       </el-col>
       <el-col :span="1.5">
@@ -61,7 +45,7 @@
           size="mini"
           :disabled="multiple"
           @click="handleDelete"
-          v-hasPermi="['workflow:marketing:remove']"
+          v-hasPermi="['workflow:tasks:remove']"
         >删除</el-button>
       </el-col>
       <el-col :span="1.5">
@@ -71,39 +55,19 @@
           icon="el-icon-download"
           size="mini"
           @click="handleExport"
-          v-hasPermi="['workflow:marketing:export']"
+          v-hasPermi="['workflow:tasks:export']"
         >导出</el-button>
       </el-col>
       <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
 
-    <el-table v-loading="loading" :data="marketingList" @selection-change="handleSelectionChange">
+    <el-table v-loading="loading" :data="tasksList" @selection-change="handleSelectionChange">
       <el-table-column type="selection" width="55" align="center" />
-      <el-table-column label="活动名称" align="center" prop="name" />
-      <el-table-column label="排序" align="center" prop="sort" />
-      <el-table-column label="创建时间" align="center" prop="creattime" width="180">
-        <template slot-scope="scope">
-          <span>{{ parseTime(scope.row.creattime, '{y}-{m}-{d}') }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="终止时间" align="center" prop="stopTime" width="180">
-        <template slot-scope="scope">
-          <span>{{ parseTime(scope.row.stopTime, '{y}-{m}-{d}') }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="显示位置" align="center" prop="displayposition" />
-      <el-table-column label="BANNER图" align="center" prop="bannerImage" width="100">
-        <template slot-scope="scope">
-          <image-preview :src="scope.row.bannerImage" :width="50" :height="50"/>
-        </template>
-      </el-table-column>
-      <el-table-column label="H5链接" align="center" prop="h5Url" />
-      <el-table-column label="活动规则" align="center" prop="rule" />
-      <el-table-column label="状态" align="center" prop="state">
-        <template slot-scope="scope">
-          <dict-tag :options="dict.type.sys_job_status" :value="scope.row.state"/>
-        </template>
-      </el-table-column>
+      <el-table-column label="${comment}" align="center" prop="id" />
+      <el-table-column label="任务名称" align="center" prop="name" />
+      <el-table-column label="可完成任务" align="center" prop="function" />
+      <el-table-column label="参与限制" align="center" prop="joinLimit" />
+      <el-table-column label="次数限制" align="center" prop="triesLimit" />
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template slot-scope="scope">
           <el-button
@@ -111,14 +75,14 @@
             type="text"
             icon="el-icon-edit"
             @click="handleUpdate(scope.row)"
-            v-hasPermi="['workflow:marketing:edit']"
+            v-hasPermi="['workflow:tasks:edit']"
           >修改</el-button>
           <el-button
             size="mini"
             type="text"
             icon="el-icon-delete"
             @click="handleDelete(scope.row)"
-            v-hasPermi="['workflow:marketing:remove']"
+            v-hasPermi="['workflow:tasks:remove']"
           >删除</el-button>
         </template>
       </el-table-column>
@@ -132,29 +96,20 @@
       @pagination="getList"
     />
 
-    <!-- 添加或修改营销活动对话框 -->
+    <!-- 添加或修改营销任务对话框 -->
     <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
       <el-form ref="form" :model="form" :rules="rules" label-width="80px">
-        <el-form-item label="活动名称" prop="name">
-          <el-input v-model="form.name" placeholder="请输入活动名称" />
+        <el-form-item label="任务名称" prop="name">
+          <el-input v-model="form.name" placeholder="请输入任务名称" />
         </el-form-item>
-        <el-form-item label="排序" prop="sort">
-          <el-input v-model="form.sort" placeholder="请输入排序" />
+        <el-form-item label="可完成任务" prop="function">
+          <el-input v-model="form.function" placeholder="请输入可完成任务" />
         </el-form-item>
-        <el-form-item label="显示位置" prop="displayposition">
-          <el-input v-model="form.displayposition" placeholder="请输入显示位置" />
+        <el-form-item label="参与限制" prop="joinLimit">
+          <el-input v-model="form.joinLimit" placeholder="请输入参与限制" />
         </el-form-item>
-        <el-form-item label="BANNER图">
-          <image-upload v-model="form.bannerImage"/>
-        </el-form-item>
-        <el-form-item label="H5链接" prop="h5Url">
-          <el-input v-model="form.h5Url" placeholder="请输入H5链接" />
-        </el-form-item>
-        <el-form-item label="活动规则" prop="rule">
-          <el-input v-model="form.rule" placeholder="请输入活动规则" />
-        </el-form-item>
-        <el-form-item label="状态" prop="state">
-          <el-input v-model="form.state" placeholder="请输入状态" />
+        <el-form-item label="次数限制" prop="triesLimit">
+          <el-input v-model="form.triesLimit" placeholder="请输入次数限制" />
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -166,11 +121,10 @@
 </template>
 
 <script>
-import { listMarketing, getMarketing, delMarketing, addMarketing, updateMarketing } from "@/api/workflow/marketing";
+import { listTasks, getTasks, delTasks, addTasks, updateTasks } from "@/api/workflow/tasks";
 
 export default {
-  name: "Marketing",
-  dicts: ['sys_job_status'],
+  name: "Tasks",
   data() {
     return {
       // 遮罩层
@@ -185,8 +139,8 @@ export default {
       showSearch: true,
       // 总条数
       total: 0,
-      // 营销活动表格数据
-      marketingList: [],
+      // 营销任务表格数据
+      tasksList: [],
       // 弹出层标题
       title: "",
       // 是否显示弹出层
@@ -196,8 +150,6 @@ export default {
         pageNum: 1,
         pageSize: 10,
         name: null,
-        creattime: null,
-        stopTime: null,
       },
       // 表单参数
       form: {},
@@ -210,11 +162,11 @@ export default {
     this.getList();
   },
   methods: {
-    /** 查询营销活动列表 */
+    /** 查询营销任务列表 */
     getList() {
       this.loading = true;
-      listMarketing(this.queryParams).then(response => {
-        this.marketingList = response.rows;
+      listTasks(this.queryParams).then(response => {
+        this.tasksList = response.rows;
         this.total = response.total;
         this.loading = false;
       });
@@ -229,14 +181,9 @@ export default {
       this.form = {
         id: null,
         name: null,
-        sort: null,
-        creattime: null,
-        stopTime: null,
-        displayposition: null,
-        bannerImage: null,
-        h5Url: null,
-        rule: null,
-        state: null
+        function: null,
+        joinLimit: null,
+        triesLimit: null
       };
       this.resetForm("form");
     },
@@ -260,16 +207,16 @@ export default {
     handleAdd() {
       this.reset();
       this.open = true;
-      this.title = "添加营销活动";
+      this.title = "添加营销任务";
     },
     /** 修改按钮操作 */
     handleUpdate(row) {
       this.reset();
       const id = row.id || this.ids
-      getMarketing(id).then(response => {
+      getTasks(id).then(response => {
         this.form = response.data;
         this.open = true;
-        this.title = "修改营销活动";
+        this.title = "修改营销任务";
       });
     },
     /** 提交按钮 */
@@ -277,13 +224,13 @@ export default {
       this.$refs["form"].validate(valid => {
         if (valid) {
           if (this.form.id != null) {
-            updateMarketing(this.form).then(response => {
+            updateTasks(this.form).then(response => {
               this.$modal.msgSuccess("修改成功");
               this.open = false;
               this.getList();
             });
           } else {
-            addMarketing(this.form).then(response => {
+            addTasks(this.form).then(response => {
               this.$modal.msgSuccess("新增成功");
               this.open = false;
               this.getList();
@@ -295,8 +242,8 @@ export default {
     /** 删除按钮操作 */
     handleDelete(row) {
       const ids = row.id || this.ids;
-      this.$modal.confirm('是否确认删除营销活动编号为"' + ids + '"的数据项？').then(function() {
-        return delMarketing(ids);
+      this.$modal.confirm('是否确认删除营销任务编号为"' + ids + '"的数据项？').then(function() {
+        return delTasks(ids);
       }).then(() => {
         this.getList();
         this.$modal.msgSuccess("删除成功");
@@ -304,9 +251,9 @@ export default {
     },
     /** 导出按钮操作 */
     handleExport() {
-      this.download('workflow/marketing/export', {
+      this.download('workflow/tasks/export', {
         ...this.queryParams
-      }, `marketing_${new Date().getTime()}.xlsx`)
+      }, `tasks_${new Date().getTime()}.xlsx`)
     }
   }
 };
